4c0e987b7a7a1ca38cb3f276c60285dd2a8abb27
suraj8800 <biswassuraj205@gmail.com>
2026-02-14 20:55:34 +0530

    initial commit for activity service microservice

---
+++ activityservice/src/main/java/com/fitness/activityservice/Service/ActivityService.java
@@ -0,0 +1,43 @@
+package com.fitness.activityservice.Service;
+
+import com.fitness.activityservice.ActivityRepositiory;
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepositiory activityRepositiory;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrices(request.getAdditionalMetrices())
+                .build();
+
+        Activity savedActivity = activityRepositiory.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrices(activity.getAdditionalMetrices());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+    }
+}
